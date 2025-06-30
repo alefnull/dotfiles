dcd8c70532cdcd864d9e536fabc836757328111f
alefnull <nullalef@gmail.com>
2025-06-29 20:38:56 -0400

    add fish config

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias cls='clear && fastfetch'
+alias ls='eza --color=auto -lh'
+alias ll='eza --color=auto -lha'
+alias cat='bat'
