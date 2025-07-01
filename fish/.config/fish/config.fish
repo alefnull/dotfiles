384c2c2139d86f555a14df24af5b9a339fe2ef3b
alefnull <nullalef@gmail.com>
2025-06-30 22:13:02 -0400

    updates

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,7 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set -Ux EDITOR nvim
+
 alias cls='clear && fastfetch'
-alias ls='eza --color=auto -lh'
-alias ll='eza --color=auto -lha'
+#alias ls='eza --color=auto -lah'
+alias ls='y'
+alias ll='y'
 alias cat='bat'
