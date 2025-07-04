d917c526dae453a5473f2fe49d236330536a6bca
alefnull <nullalef@gmail.com>
2025-07-04 15:16:42 -0400

    add zellij

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,8 @@
+set -gx XDG_CONFIG_HOME "$HOME/.config"
+set -gx XDG_DATA_HOME "$HOME/.local/share"
+set -gx XDG_STATE_HOME "$HOME/.local/state"
+set -gx XDG_CACHE_HOME "$HOME/.cache"
+
 set -gx EDITOR nvim
 set -gx GIT_EDITOR nvim
 
@@ -9,3 +14,6 @@ alias ll='eza --color=auto --git -lah'
 alias cat='bat'
 alias lg='lazygit'
 alias lf='y'
+alias zj='zellij --layout compact'
+
+zellij_update_tabname
