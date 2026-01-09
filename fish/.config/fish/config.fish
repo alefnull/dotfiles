92c876183f4a853b17ebee844b4bea43aa8cbf2b
alefnull <nullalef@gmail.com>
2026-01-09 14:54:11 -0500

    01092026 update (add dcli/arch-config)

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,6 +4,7 @@ set -gx XDG_STATE_HOME "$HOME/.local/state"
 set -gx XDG_CACHE_HOME "$HOME/.cache"
 
 set -gx EDITOR nvim
+set -gx VISUAL nvim
 set -gx GIT_EDITOR nvim
 
 set -gx DOTFILES "$HOME/dotfiles"
